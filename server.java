c2b6a5740a03bb269407189e613df9d494b47e3f
Shreya Sharma <34606101+ShreyaSharma99@users.noreply.github.com>
2019-09-02 23:24:07 +0530

    New

--- server.java
+++ server.java
@@ -3,35 +3,93 @@ import java.net.*;
 
 class TCPServer { 
 
+  HashMap<String, Socket> socketMap = new HashMap<>(); 
+  HashMap<String, Integer> keyMap = new HashMap<>(); 
+
+  public static boolean parseMessage(String message, Socket clientSocket, int key){
+    String line;
+    String username;
+
+    if (message.hasNextLine()) {
+            line = message.nextLine();
+            String word = line.next();
+
+            if(!word.equals("SEND")){
+              System.out.println("SEND token missing !");
+              System.ot.println("ERROR 103 Header incomplete\n \n");
+              return false;
+            }
+
+            if(!line.hasNext()){
+              System.out.println("Username missing !");
+              System.ot.println("ERROR 103 Header incomplete\n \n");
+              return false;
+            }
+            
+            word = line.next();
+
+            if(!socketMap.containsKey(word)){
+              System.out.println("ERROR 101 No user registered\n \n");
+            }
+            else {
+              clientSocket = socketMap.get(word);
+              key = keyMap.get(word);
+            }
+
+            if(message.hasNextLine()){
+              line = message.nextLine();
+              word = line.next();
+              if(!word,equals("Content-length:"))
+                return false;
+            }
+
+            if(!message.hasNextLine()){
+              System.out.println("Wrong format! Leave one line before the starting of the message.");
+              System.ot.println("ERROR 103 Header incomplete\n \n");
+              return false;
+            }
+            else{
+              line = message.nextLine();
+              if(line.hasNext()){
+                System.out.println("Wrong format! Leave one line before the starting of the message.");
+                System.ot.println("ERROR 103 Header incomplete\n \n");
+                return false;
+              }
+            }
+    }
+    else{
+      System.out.println("Empty message!");
+      System.ot.println("ERROR 103 Header incomplete\n \n");
+      return false;
+    }
+     return true;
+  }
+
   public static void main(String argv[]) throws Exception 
     { 
-      String clientSentence; 
-      String capitalizedSentence; 
+      String clientMessage; 
+      String forwardMessage; 
+      String replyFromServer;
 
       ServerSocket welcomeSocket = new ServerSocket(6789); 
   
-  
       Socket connectionSocket = welcomeSocket.accept(); 
 
-      BufferedReader inFromClient = 
-           new BufferedReader(new
-           InputStreamReader(connectionSocket.getInputStream())); 
+      BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
+
+      DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream()); 
+
+     while(true) { 
+         clientMessage = inFromClient.readLine(); 
+
+	   System.out.println(clientMessage);
 
 
-      DataOutputStream  outToClient = 
-           new DataOutputStream(connectionSocket.getOutputStream()); 
+           forwardMessage = clientMessage.toUpperCase() + '\n'; 
 
-       while(true) { 
-           clientSentence = inFromClient.readLine(); 
-
-	   System.out.println(clientSentence);
-
-           capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
-
-           outToClient.writeBytes(capitalizedSentence); 
+           outToClient.writeBytes(forwardMessage); 
         } 
     } 
 } 
  
-
 
