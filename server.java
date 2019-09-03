20f9101b302aee415c47c6f5de1a2c45b2b7906a
Shreya Sharma <34606101+ShreyaSharma99@users.noreply.github.com>
2019-09-03 18:58:17 +0530

    Server without object

--- server.java
+++ server.java
@@ -1,96 +1,200 @@
 import java.io.*; 
 import java.net.*; 
-
+ 
 class TCPServer { 
 
-  HashMap<String, Socket> socketMap = new HashMap<>(); 
-  HashMap<String, Integer> keyMap = new HashMap<>(); 
+  public static HashMap<String, Socket> sendSocketMap = new HashMap<>(); 
+  public static HashMap<String, Socket> recSocketMap = new HashMap<>(); 
+  public static HashMap<String, Integer> keyMap = new HashMap<>();
 
-  public static boolean parseMessage(String message, Socket clientSocket, int key){
-    String line;
-    String username;
+  public String[] readMessage(BufferedReader inFromClient, Socket connectionSocket){
 
-    if (message.hasNextLine()) {
-            line = message.nextLine();
-            String word = line.next();
-
-            if(!word.equals("SEND")){
-              System.out.println("SEND token missing !");
-              System.ot.println("ERROR 103 Header incomplete\n \n");
-              return false;
-            }
-
-            if(!line.hasNext()){
-              System.out.println("Username missing !");
-              System.ot.println("ERROR 103 Header incomplete\n \n");
-              return false;
-            }
-            
-            word = line.next();
-
-            if(!socketMap.containsKey(word)){
-              System.out.println("ERROR 101 No user registered\n \n");
-            }
-            else {
-              clientSocket = socketMap.get(word);
-              key = keyMap.get(word);
-            }
-
-            if(message.hasNextLine()){
-              line = message.nextLine();
-              word = line.next();
-              if(!word,equals("Content-length:"))
-                return false;
-            }
-
-            if(!message.hasNextLine()){
-              System.out.println("Wrong format! Leave one line before the starting of the message.");
-              System.ot.println("ERROR 103 Header incomplete\n \n");
-              return false;
-            }
-            else{
-              line = message.nextLine();
-              if(line.hasNext()){
-                System.out.println("Wrong format! Leave one line before the starting of the message.");
-                System.ot.println("ERROR 103 Header incomplete\n \n");
-                return false;
+    String clientMessage = "";
+    String serverReply = "";
+    String fwdMessage = "";
+    String errorMessage = "";
+    String username = "";
+    while(inFromClient.ready()){
+      clientMessage = inFromClient.readLine();
+      String[] word = clientMessage.split("\\s+");
+      try{
+        //when input message was for registration
+        if(word[0].equals("REGISTER")){
+          if(word[1].equals("TOSEND")){
+            username = word[2];
+            if(checkUserName(username)){
+              sendSocketMap.put(username,connectionSocket);
+              clientMessage = inFromClient.readLine();
+              if(clientMessage != null){
+                clientMessage = inFromClient.readLine();
+                if(clientMessage == null){
+                  serverReply = "REGISTERED TOSEND "+username+"\n\n";
+                  while(inFromClient.ready())
+                    clientMessage = inFromClient.readLine();
+                  break;
+                }
               }
             }
+            else{
+              serverReply = "ERROR 100 Malformed "+username+"\n\n";
+              while(inFromClient.ready())
+                clientMessage = inFromClient.readLine();
+              break;
+            }
+          }
+          else if(word[1].equals("TORECV")){
+            username = word[2];
+            if(checkUserName(username)){
+              recSocketMap.put(username,connectionSocket);
+              clientMessage = inFromClient.readLine();
+              if(clientMessage != null){
+                clientMessage = inFromClient.readLine();
+                if(clientMessage == null){
+                  serverReply = "REGISTERED TORECV "+username+"\n\n";
+                  while(inFromClient.ready())
+                    clientMessage = inFromClient.readLine();
+                  break;
+                }
+              }
+            }
+            else{
+              serverReply = "ERROR 100 Malformed "+username+"\n\n";
+              while(inFromClient.ready())
+                clientMessage = inFromClient.readLine();
+              break;
+            }
+          }
+        }
+
+        else if(word[0].equals("SEND")){
+  
+          username = word[1];
+          if(!recSocketMap.containsKey(username)){
+            serverReply = "ERROR 102 Unable to send\n\n";
+            while(inFromClient.ready())
+              clientMessage = inFromClient.readLine();
+            break;
+          }
+
+          clientMessage = inFromClient.readLine();
+          String[] word1 = clientMessage.split("\\s+");
+
+          if(word1[0].equals("Content-length:")){
+            int mssg_len = Integer.parseInt(word1[1]);
+            if(inFromClient.readLine().length() == 0){
+              String fwdMessage = inFromClient.readLine();
+              if(fwdMessage.length()!=mssg_len){
+                serverReply = "ERROR - Content length and message length mismatch !";
+                while(inFromClient.ready())
+                  clientMessage = inFromClient.readLine();
+                break;
+              }
+            } 
+          }
+        }
+
+        else if(word[0].equals("RECEIVED")){
+          while(inFromClient.ready())
+            clientMessage = inFromClient.readLine();
+          break;
+        }
+
+        else if(word[0].equals("ERROR")){
+          if(word[1].equals("103")){
+            errorMessage = "Header incomplete";
+            while(inFromClient.ready())
+              clientMessage = inFromClient.readLine();
+            break;
+          }
+        }
+
+      }catch(Exception e){
+        while(inFromClient.ready())
+              clientMessage = inFromClient.readLine();
+        serverReply = "ERROR 103 Header incomplete \n\n";
+      }
     }
-    else{
-      System.out.println("Empty message!");
-      System.ot.println("ERROR 103 Header incomplete\n \n");
-      return false;
+    String[4] answer;
+    answer[0] = serverReply;
+    answer[1] = fwdMessage;
+    answer[2] = errorMessage;
+    answer[3] = username;
+    return answer; 
+  } 
+
+  public boolean checkUserName (String un ){
+    int len = un.length();
+    //if(un.charAt(0) != '@') return false;
+    for(int i=0; i<len; i++){
+      if!((((int)un.charAt(i)>47)&&((int)un.charAt(i)<58)) || (((int)un.charAt(i)>64)&&((int)un.charAt(i)<91)) || (((int)un.charAt(i)>96)&&((int)un.charAt(i)<123)))
+        return false;
     }
-     return true;
+    return true;
   }
 
   public static void main(String argv[]) throws Exception 
     { 
-      String clientMessage; 
-      String forwardMessage; 
-      String replyFromServer;
 
       ServerSocket welcomeSocket = new ServerSocket(6789); 
-  
-      Socket connectionSocket = welcomeSocket.accept(); 
+      
+      while(true) { 
+        Socket connectionSocket = welcomeSocket.accept(); 
 
-      BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
+        DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream()); 
 
-      DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream()); 
+        SocketThread socketThread = new SocketThread(connectionSocket, inFromClient, outToClient);
 
-     while(true) { 
-         clientMessage = inFromClient.readLine(); 
+        Thread thread = new Thread(socketThread);
+        thread.run();  
 
-	   System.out.println(clientMessage);
-
-
-           forwardMessage = clientMessage.toUpperCase() + '\n'; 
-
-           outToClient.writeBytes(forwardMessage); 
-	     
-        } 
-    } 
+      }
+    }
 } 
  
+class SocketThread extends TCPServer implements Runnable {
+  String serverReply;
+  String fwdMessage; 
+  String errorMessage;
+  String username;
+  Socket connectionSocket;
+  BufferedReader inFromClient1;
+  DataOutputStream outToClient1;  //client 1 is the calling client
+   
+  SocketThread (Socket connectionSocket, BufferedReader inFromClient, DataOutputStream outToClient) {
+    this.connectionSocket = connectionSocket;
+    this.inFromClient1 = inFromClient;
+    this.outToClient1 = outToClient;
+  } 
 
+  public void run() {
+    while(true) { 
+     try {
+      String[4] str = readMessage(inFromClient1, connectionSocket);
+      serverReply = str[0];
+      fwdMessage = str[1];
+      errorMessage = str[2];
+      username = str[3];
+    
+      if(serverReply.length()!=0){
+        outToClient1.writeBytes(serverReply);
+      } 
+
+      if(fwdMessage.length()!=0 && username.length()!=0){
+        //forwrad message to receiving client
+        Socket forwardSocket = recSocketMap.get(username);
+        DataOutputStream outToClient2 = new DataOutputStream(forwardSocket.getOutputStream());
+        outToClient2.writeBytes(fwdMessage);
+        BufferedReader inFromClient2 = new BufferedReader(new InputStreamReader(forwardSocket.getInputStream()));
+
+      }
+
+     }catch(Exception e) {
+        try {
+         connectionSocket.close();
+        } catch(Exception ee) { }
+        break;
+     }
+    } 
+  }  
+}
